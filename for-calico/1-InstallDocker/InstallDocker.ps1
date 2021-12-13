404cffc260c6f0914e6373a438e10a6c7d66f89b
kai <sunkai5210770@live.com>
2021-12-13 19:05:31 +0800

    Add files via upload

--- for-calico/1-InstallDocker/InstallDocker.ps1
+++ for-calico/1-InstallDocker/InstallDocker.ps1
@@ -10,15 +10,11 @@ This script use to install docker for Windows Server.
 .EXAMPLE
 PS> .\InstallDocker.ps1
 #>
-
-function Pause(){
-  [System.Console]::Write('Press any key to restart...')
-  [void][System.Console]::ReadKey(1)
-}
+Set-Location .\1-InstallDocker
 
 try {
   Write-Host 'Try to enable container feature.' 
-  mkdir c:\offline
+  New-Item -ItemType Directory c:\offline -Force|Out-Null
   Dism /Mount-Image /ImageFile:.\install.wim /Index:1 /MountDir:c:\offline /ReadOnly
   
   Enable-WindowsOptionalFeature -Online -FeatureName "Containers" -All -Source "c:\offline" -LimitAccess -NoRestart
@@ -28,6 +24,7 @@ try {
 }
 catch {
   Write-Host 'Failed to enable container feature!!! Please contact the admin to troubleshoot.' -ForegroundColor Red
+  exit 1
 }
 
 try {
@@ -38,23 +35,36 @@ try {
   [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
   Write-Host 'Register service for docker.'
   & 'C:\Program Files\docker\dockerd.exe' --register-service
-  Write-Host 'Install finshed!' -ForegroundColor Green
+  Write-Host 'Docker install finshed!' -ForegroundColor Green
   $CniConf = @"
 {
-  "insecure-registries": [
-    "0.0.0.0/0"
-  ]
+  "insecure-registries" : ["0.0.0.0/0"],
+  "allow-nondistributable-artifacts" : ["0.0.0.0/0"]
 }
 "@
-  mkdir "C:\ProgramData\Docker\config" -Force
+  New-Item -ItemType Directory "C:\ProgramData\Docker\config" -Force|Out-Null
   $CniConf | Out-File "C:\ProgramData\Docker\config\daemon.json" -Encoding Ascii -Force
   #start-service -name docker
 }
 catch {
   Write-Host 'Failed to install docker!!! Please contact the admin to troubleshoot.' -ForegroundColor Red
+  exit 1
 }
 
-New-Item -ItemType Directory c:\k -Force|Out-Null
-pause
+
+#Create startup script to continue install
+$trigger = New-JobTrigger -AtStartup -RandomDelay 30
+$Step2Script = $(Split-Path $(Get-Location) -Parent)+"\2-PrepareK8sNode\PrepareK8sNode.ps1"
+$Step2 = "Start-Process powershell -ArgumentList $Step2Script -RedirectStandardOutput C:\k\log\K8sInstall.log -RedirectStandardError C:\k\log\K8sInstall.err"
+$Step2 = [Scriptblock]::Create($Step2)
+Register-ScheduledJob -Trigger $trigger -ScriptBlock $Step2 -Name InstallK8s
+
+$KubernetesPath = "$env:SystemDrive\k"
+$LogDir="C:\k\log"
+New-Item -Type Directory -p $KubernetesPath -Force|Out-Null
+New-Item -Type Directory -p $LogDir -Force|Out-Null
+
+Write-Host "#####After reboot#####`n#####Continue to 2-PrepareK8sNode#####`n" -ForegroundColor Yellow
+start-sleep 6
 
 Restart-Computer -Force 
